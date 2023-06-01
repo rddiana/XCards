2b1dac5b2d29bc4636aa01654b22dbb3a349413d
Diana <rd.diana@mail.ru>
2023-06-01 14:27:06 +0700

    ...

---
+++ app/src/main/java/com/example/xcards/domain/adapters/DisplayingCardsAdapter.kt
@@ -0,0 +1,98 @@
+package com.example.xcards.domain.adapters
+
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isGone
+import androidx.recyclerview.widget.RecyclerView
+import com.example.xcards.R
+import com.example.xcards.data.CardContentData
+
+class DisplayingCardsAdapter(
+    val context: Context,
+    val cardContentList: ArrayList<CardContentData>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return DisplayingCardHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val cardContentData = cardContentList[position]
+
+        (holder as DisplayingCardHolder).textQuestion.text = cardContentData.question
+        holder.textAnswer.text = cardContentData.answer
+
+        holder.viewAnswer.setOnClickListener {
+            flipCard(context, holder.viewQuestion, it)
+        }
+
+        holder.viewQuestion.setOnClickListener {
+            flipCard(context, holder.viewAnswer, it)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return cardContentList.size
+    }
+
+    inner class DisplayingCardHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var viewQuestion: View
+        var textQuestion: TextView
+        var viewAnswer: View
+        var textAnswer: TextView
+
+        init {
+            viewQuestion = view.findViewById(R.id.view_question)
+            textQuestion = view.findViewById(R.id.cardQuestionText)
+            viewAnswer = view.findViewById(R.id.view_answer)
+            textAnswer = view.findViewById(R.id.cardAnswerText)
+        }
+    }
+
+    private fun removeSwipedItem() {
+        cardContentList.removeAt(0)
+        notifyDataSetChanged()
+    }
+
+    private fun flipCard(context: Context, visibleView: View, inVisibleView: View) {
+        try {
+            visibleView.visibility = View.VISIBLE
+
+            val scale = context.resources.displayMetrics.density
+            val cameraDist = 8000 * scale
+            visibleView.cameraDistance = cameraDist
+            inVisibleView.cameraDistance = cameraDist
+
+            val flipOutAnimatorSet =
+                AnimatorInflater.loadAnimator(
+                    context,
+                    R.animator.flip_out
+                ) as AnimatorSet
+
+            flipOutAnimatorSet.setTarget(inVisibleView)
+
+            val flipInAnimationSet =
+                AnimatorInflater.loadAnimator(
+                    context,
+                    R.animator.flip_in
+                ) as AnimatorSet
+
+            flipInAnimationSet.setTarget(visibleView)
+            flipOutAnimatorSet.start()
+            flipInAnimationSet.start()
+            flipInAnimationSet.doOnEnd {
+                inVisibleView.isGone = true
+            }
+        } catch (e: Exception) {
+            Log.d("animator exception", e.message.toString())
+        }
+    }
+}
--- app/src/main/java/com/example/xcards/presentation/DisplayingCardsFragment.kt
+++ app/src/main/java/com/example/xcards/presentation/DisplayingCardsFragment.kt
@@ -1,29 +1,25 @@
 package com.example.xcards.presentation
 
-import android.animation.AnimatorInflater
-import android.animation.AnimatorSet
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.animation.doOnEnd
-import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import com.example.xcards.R
 import com.example.xcards.data.CardContentData
 import com.example.xcards.databinding.FragmentDisplayingCardsBinding
+import com.example.xcards.domain.adapters.DisplayingCardsAdapter
 import com.example.xcards.domain.useCase.FirebaseDatabaseUtils
-
 
 class DisplayingCardsFragment(
     private val listCardDataContent: List<CardContentData>
 ) : Fragment() {
     private lateinit var binding: FragmentDisplayingCardsBinding
     private lateinit var database: FirebaseDatabaseUtils
+
+    private lateinit var adapter: DisplayingCardsAdapter
 
     private var position = 0
 
@@ -34,16 +30,11 @@ class DisplayingCardsFragment(
         binding = FragmentDisplayingCardsBinding.inflate(layoutInflater)
         database = FirebaseDatabaseUtils(requireContext().applicationContext)
 
-        binding.viewQuestion.findViewById<TextView>(R.id.cardQuestionText).text = listCardDataContent[0].question
-        binding.viewAnswer.findViewById<TextView>(R.id.cardAnswerText).text = listCardDataContent[0].answer
+        adapter = DisplayingCardsAdapter(
+            requireContext(),
+            ArrayList(listCardDataContent)
+        )
 
-        binding.viewAnswer.setOnClickListener {
-            flipCard(requireContext(), binding.viewQuestion, it)
-        }
-
-        binding.viewQuestion.setOnClickListener {
-            flipCard(requireContext(), binding.viewAnswer, it)
-        }
 
         binding.backArrow.setOnClickListener {
             parentFragmentManager.beginTransaction().replace(
@@ -52,40 +43,6 @@ class DisplayingCardsFragment(
         }
 
         return binding.root
-    }
-
-    private fun flipCard(context: Context, visibleView: View, inVisibleView: View) {
-        try {
-            visibleView.visibility = View.VISIBLE
-
-            val scale = context.resources.displayMetrics.density
-            val cameraDist = 8000 * scale
-            visibleView.cameraDistance = cameraDist
-            inVisibleView.cameraDistance = cameraDist
-
-            val flipOutAnimatorSet =
-                AnimatorInflater.loadAnimator(
-                    context,
-                    R.animator.flip_out
-                ) as AnimatorSet
-
-            flipOutAnimatorSet.setTarget(inVisibleView)
-
-            val flipInAnimationSet =
-                AnimatorInflater.loadAnimator(
-                    context,
-                    R.animator.flip_in
-                ) as AnimatorSet
-
-            flipInAnimationSet.setTarget(visibleView)
-            flipOutAnimatorSet.start()
-            flipInAnimationSet.start()
-            flipInAnimationSet.doOnEnd {
-                inVisibleView.isGone = true
-            }
-        } catch (e: Exception) {
-            Log.d("animator exception", e.message.toString())
-        }
     }
 
     private fun onRightToLeftSwipe(view: View) {
